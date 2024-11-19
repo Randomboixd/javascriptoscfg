f13e0146ec35b4a56c3f92188604c3da844ec82a
Randomboixd <100033803+Randomboixd@users.noreply.github.com>
2024-11-19 20:17:47 +0100

    wait no i wait.

--- src/lib/joscfg-ostree/helpers/writeShellScriptBin.ts
+++ src/lib/joscfg-ostree/helpers/writeShellScriptBin.ts
@@ -13,7 +13,7 @@ export default (shellScriptName: string, contents: string): Package => {
     }
     
 
-    fs.writeFileSync(path.join(".", "temp", "#!/bin/bash\n"+shellScriptName), contents)
+    fs.writeFileSync(path.join(".", "temp", shellScriptName), "#!/bin/bash\n"+contents)
 
     return {
         name: shellScriptName,
